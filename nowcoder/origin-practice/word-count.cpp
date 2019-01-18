84bc2b7e5f10d338cd00b16ae1ea460a754112f6
andy <andy_viky@163.com>
2019-01-18 15:14:37 +0800

    ac word-count

---
+++ nowcoder/origin-practice/word-count.cpp
@@ -0,0 +1,32 @@
+//
+// Created by 杨霖 on 2019/1/18.
+//
+
+#include <cstdio>
+#include "string.h"
+using namespace std;
+#define MAXSIZE 10000
+
+int main() {
+    int i=0, j=0, count=0;
+    char phrase[MAXSIZE], tempC;
+    while(true) {
+        scanf("%c", &tempC);
+        phrase[i++] = tempC;
+        if(tempC == '.') break;
+    }
+    while(phrase[j]) {
+        count++;
+        if(phrase[j] == '.') {
+            printf("%d", count-1);
+            break;
+        };  //最后一个 . 退出循环
+        if(phrase[j] == ' ') {
+            printf("%d ", count-1);
+            count = 0;
+        }
+        j++;
+    }
+    return 0;
+}
+
