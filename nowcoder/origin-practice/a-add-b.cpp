09b5725439426d1c172a359ee496a7284e724cb9
andy <andy_viky@163.com>
2019-01-28 21:35:31 +0800

    ac a+b

---
+++ nowcoder/origin-practice/a-add-b.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 杨霖 on 2019/1/28.
+//
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a, b;
+    while(scanf("%d %d", &a, &b) != EOF) printf("%d\n", a+b);
+    return 0;
+}
