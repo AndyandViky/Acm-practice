a65c5b3807366b11cdd8d9356abd560c9ca29ac1
andy <andy_viky@163.com>
2019-02-05 23:30:16 +0800

    fix base-1005 logic

--- base-lever/1005.cpp
+++ base-lever/1005.cpp
@@ -10,35 +10,46 @@ using namespace std;
 #define MAXCOL 20
 
 bool cmp(int arr[], int brr[], int n) {
-    int i;
+    int i, index1 = 0, index2 = 0;
     for(i=0; i<n; i++) {
-        if(arr[i] == 0) return false;
-        if(arr[i] == brr[0]) {
-            return true;
+        if(arr[i] == 0) {
+            index1 = i-1;
+            break;
         }
     }
-    return false;
+    for(i=0; i<n; i++) {
+        if(brr[i] == 0) {
+            index2 = i-1;
+            break;
+        }
+    }
+    if(index1 < index2) return false;
+    else {
+        while(index2 >= 0) {
+            if(arr[index1--] != brr[index2--]) return false;
+        }
+    }
+    return true;
 }
 int main() {
     int N, cinData[MAXROW] = {0}, i, j, count = 0, result[MAXROW], rCount = 0, midData[MAXROW][MAXCOL];
-    for(i=0; i<MAXROW; i++) {
-        for(j=0; j<MAXCOL; j++) {
+    for (i = 0; i < MAXROW; i++) {
+        for (j = 0; j < MAXCOL; j++) {
             midData[i][j] = 0;
         }
     }
     scanf("%d", &N);
-    for(i=0; i<N; i++) {
+    for (i = 0; i < N; i++) {
         scanf("%d", &cinData[i]);
         midData[i][0] = cinData[i];
     }
 
-    for(i=0; i<N; i++) {
+    for (i = 0; i < N; i++) {
         count = 0;
-        while(cinData[i] > 1) {
-            if(cinData[i]%2 == 0) {
-                cinData[i]/=2;
-            }
-            else {
+        while (cinData[i] > 1) {
+            if (cinData[i] % 2 == 0) {
+                cinData[i] /= 2;
+            } else {
                 cinData[i] = (3 * cinData[i] + 1) / 2;
             }
             count++;
@@ -47,29 +58,28 @@ int main() {
     }
     // 找出所有结果数组中的差
     int tags[MAXCOL] = {0};
-    for(i=0; i<N; i++) {
-        if(tags[i] == 1) continue;
-        for(j=0; j<N; j++) {
-            if(i == j) continue;
-            if(cmp(midData[i], midData[j], MAXCOL)) {
+    for (i = 0; i < N; i++) {
+        if (tags[i] == 1) continue;
+        for (j = 0; j < N; j++) {
+            if (i == j) continue;
+            if (cmp(midData[i], midData[j], MAXCOL)) {
                 tags[j] = 1;
             }
         }
     }
-    for(i=0; i<MAXCOL; i++) {
-        if(tags[i] != 1) {
+    for (i = 0; i < MAXCOL; i++) {
+        if (tags[i] != 1) {
             result[rCount] = midData[i][0];
             rCount++;
         }
     }
     // 排序
-    sort(result, result+rCount, greater<int>());
-    for(i=0; i<rCount; i++) {
-        if(result[i] != 0) {
-            if(i != rCount-1) printf("%d ", result[i]);
+    sort(result, result + rCount, greater<int>());
+    for (i = 0; i < rCount; i++) {
+        if (result[i] != 0) {
+            if (result[i + 1] != 0) printf("%d ", result[i]);
             else printf("%d", result[i]);
         }
     }
     return 0;
 }
-
